b050adfbc2aec218e01f1f3ebdb0757ee4c3b681
jmlakshmisagar <jmlakshmisagar@gmail.com>
2025-12-01 13:47:14 +0530

    Clean Commit, initialized the project with quickstart

---
+++ Tax-Calculation-Application/src/main/java/tax/calculation/application/App.java
@@ -0,0 +1,10 @@
+package tax.calculation.application;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
